e80e10070ddf051cbfb27c9d3b7b9679c3251b26
Akbarhon Avazkhodjayev <Akbarhon.tbs@gmail.com>
2020-01-01 20:40:36 -0500

    adding functionality to pig game

--- Section_4_projects/DOM-pig-game/starter/app.js
+++ Section_4_projects/DOM-pig-game/starter/app.js
@@ -9,24 +9,105 @@ GAME RULES:
 
 */
 
+var scores, roundScore, activePlayer;
+
+init();
 
 
 
 
+//Event Listener documentation can be found on  ---- https://developer.mozilla.org/en-US/docs/Web/Events
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    
+    // 1. Random Number
+    var dice = Math.floor((Math.random() * 6) + 1);   
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'
+
+    //3. Update the round score IF the rolled number was NOT a 1
+
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else { 
+        //if dice is equal to  run this code
+        nextPlayer();
+    }
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+
+    //Add current score to GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    //Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    //Check if player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+    } else {
+        nextPlayer();
+    }
+    
+  
+
+})
+
+
+function nextPlayer() {
+    //Ternary Operator
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    //toggles the active class which indicates throught HTML and CSS the current player
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //Same concept as above code 
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
 
 
 
+    document.querySelector('.dice').style.display = 'none';
+
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+    
 
 
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
 
+    //changes image property to none
+    document.querySelector('.dice').style.display = 'none'
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
 
-
-
-
-
-
-
-
+}
 
 
 
