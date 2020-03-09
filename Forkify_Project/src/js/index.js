571c44a991337263b89620329c16e22a22331342
Akbarhon Avazkhodjayev <Akbarhon.tbs@gmail.com>
2020-03-09 16:58:28 -0400

    npm,babel, and webpack installation

--- Forkify_Project/src/js/index.js
+++ Forkify_Project/src/js/index.js
@@ -1,1 +1,37 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView'
+import {elements} from './views/base';
+
+/** Global State of the App
+ * - Search Object
+ * - Current Recipe
+ * - Shopping list Object
+ * - Liked Recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput(); //TODO
+    
+    
+    if(query) {
+        // 2) New Search Object and add to State
+        state.search = new Search(query)
+        console.log(state)
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        searchView.renderResults(state.search.result);
+    }
+}
+
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
