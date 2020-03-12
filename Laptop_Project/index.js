6b585fab81fca0ef00b4cfde716120aa236dee84
Akbarhon Avazkhodjayev <Akbarhon.tbs@gmail.com>
2020-03-12 12:48:37 -0400

    added routing by using http, url and fs core modules in node.js

--- Laptop_Project/index.js
+++ Laptop_Project/index.js
@@ -1,11 +1,29 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
+console.log(laptopData.length)
 
 const server = http.createServer((req, res) => {
-    console.log('Someone did access the server!');
+
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
+    if (pathName === '/products' || pathName === '') {
+        res.writeHead(200, { 'Content-type' : 'text/html'});
+        res.end('This is the PRODUCTS page');
+    } else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, { 'Content-type' : 'text/html'});
+        res.end(`This is the LAPTOP page for laptop ${id}`);
+    } else  {
+        res.writeHead(404, { 'Content-type' : 'text/html'});
+        res.end('URL NOT FOUND');
+
+    }
+
+    
 });
 
 server.listen(8080, '127.0.0.1', () => {
